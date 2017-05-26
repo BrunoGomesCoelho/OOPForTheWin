a49e32190c90ee31ca8277648b9fabffd2950379
Bruno Gomes Coelho <brasinhacoelho@hotmail.com>
2017-05-26 14:56:34 -0300

    Estruturação do projeto & exemplo de GUI básica(sample)

---
+++ src/sample/Controller.java
@@ -0,0 +1,9 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+	public void pressButton(ActionEvent event) {
+		System.out.println("Chegando mais proximo de alcançar Cyrillo!");
+	}
+}
