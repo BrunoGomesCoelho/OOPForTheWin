7daa9a585a3178bb8e506ec0b95a6df390d60731
martchello <marcello_1000@yahoo.com.br>
2017-06-19 23:59:23 -0300

    copyright

--- src/GUI/About/AboutController.java
+++ src/GUI/About/AboutController.java
@@ -13,6 +13,7 @@ import java.util.ResourceBundle;
 public class AboutController implements Initializable {
 
     @FXML Label text;
+    @FXML Label copyright_text;
 
     // There are about 77 chars per line
     private String about = "A photo edit software done by Bruno Coelho, " +
@@ -21,9 +22,17 @@ public class AboutController implements Initializable {
             "Done for the discipline OOP at the " +
             "University of São Paulo in June 2017";
 
+    private String copy = " Permission is granted to copy, distribute and/or modify this document\n" +
+            "    under the terms of the GNU Free Documentation License, Version 1.3\n" +
+            "    or any later version published by the Free Software Foundation;\n" +
+            "    with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts.\n" +
+            "    A copy of the license is included in the section entitled \"GNU\n" +
+            "    Free Documentation License\"";
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         text.setText(about);
+            copyright_text.setText(copy);
     }
 
 }
